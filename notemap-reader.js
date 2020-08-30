42a4508b8fa376d6994ece1934ca08bd3414b969
Suyooo <me@suyo.be>
2020-08-30 22:50:44 +0200

    Hey wow this one is not Abandonware so I can put a license on it

--- notemap-reader.js
+++ notemap-reader.js
@@ -1,3 +1,21 @@
+/*
+This module creates the HTML code for the note map timeline and panels for gimmick/AC info, used for the map DB pages.
+Copyright (C) 2020 Suyooo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const fs = require('fs');
 
 function capFirstLetter(s) {
