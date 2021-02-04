a37a43c3bd89b42d48e5a59e0a3de1517a7da499
Suyooo <me@suyo.be>
2021-02-04 01:09:21 +0100

    Note map interactions now work on the regular lives

--- js/mapdb.js
+++ js/mapdb.js
@@ -18,27 +18,27 @@ $(function () {
     M.AutoInit();
     let tabs = M.Tabs.getInstance($("nav .tabs")[0]);
     tabs.options.onShow = function(e) {
-        let tab = $(e);
-        window.location.hash = currentPage = tab.attr("id").substring(4);
-        if (tab.data("loaded") === undefined) {
-            tab.data("loaded", 1);
-            let type = tab.data("type");
+        let page = $(e);
+        window.location.hash = currentPage = page.attr("id").substring(4);
+        if (page.data("loaded") === undefined) {
+            page.data("loaded", 1);
+            let type = page.data("type");
             if (type === "fixed") {
                 // tab already has content - don't do anything
-                tab.removeClass("unloaded");
+                page.removeClass("unloaded");
                 return;
             }
 
             // Load tab content, delay initialization until loaded
-            tab.load(tab.attr("id").substring(4) + ".html", function() {
-                tab.removeClass("unloaded");
+            page.load(page.attr("id").substring(4) + ".html", function() {
+                page.removeClass("unloaded");
 
                 if (type === "free") {
-                    // do initialization for a Free Live collection (map collapsibles on first level)
+                    doFreeLiveInit(page);
                 } else if (type === "dlp") {
                     // do initialization for a DLP floor collection (map collapsibles on second level)
                 } else if (type === "top") {
-                    $(".rankingtable", tab).each(function () {
+                    $(".rankingtable", page).each(function () {
                         let table = $(this);
                         $(".btn", this).click(function () {
                             table.addClass("open");
@@ -54,71 +54,67 @@ $(function () {
     }
 });
 
-function doTabInit() {
-    $(".collapsible").each(function () {
+function doFreeLiveInit(page) {
+    $(".tabs", page).tabs();
+    $(".collapsible", page).collapsible().each(function () {
         let collapsible = M.Collapsible.getInstance(this);
-        if (IS_MAP_DB) {
-            // notemap database
-            let tab_elements = $(".tabs", this);
-            let story_tabs = undefined;
-            if (tab_elements.length === 0) return true; // page help collapsible
-            if (tab_elements.length > 1) story_tabs = M.Tabs.getInstance(tab_elements[1]);
-            let tabs = M.Tabs.getInstance(tab_elements[0]);
 
-            // materialize doesn't set indicator positions inside hidden elements, so we'll do it ourselves on open
-            // we can also use it to set the location anchor to link to that live difficulty
-            collapsible.options.onOpenStart = function () {
-                let tabelement = $(".tabs", this.el)[0];
-                fixTabIndicator(tabelement);
-                let activetablink = $(".active", tabelement);
+        let tab_elements = $(".tabs", this);
+        let story_tabs = undefined;
+        if (tab_elements.length > 1) story_tabs = M.Tabs.getInstance(tab_elements[1]);
+        let tabs = M.Tabs.getInstance(tab_elements[0]);
 
-                if (activetablink.attr("href").endsWith("story")) {
-                    window.location.hash = "live" + $(".active", "#" + activetablink.attr("href").substring(1)).attr("href").substring(1);
-                } else {
-                    window.location.hash = "live" + activetablink.attr("href").substring(1);
-                }
-            };
+        // materialize doesn't set indicator positions inside hidden elements, so we'll do it ourselves on open
+        // we can also use it to set the location anchor to link to that live difficulty
+        collapsible.options.onOpenStart = function () {
+            let tabelement = $(".tabs", this.el)[0];
+            fixTabIndicator(tabelement);
+            let activetablink = $(".active", tabelement);
 
-            tabs.options.onShow = function (e) {
-                if ($(e).attr("id").endsWith("story")) {
-                    M.Tabs.getInstance($(".tabs", e)[0]).updateTabIndicator();
-                    window.location.hash = "live" + $(".active", e).attr("href").substring(1);
-                } else {
-                    window.location.hash = "live" + $(e).attr("id");
-                }
-            };
-
-            if (story_tabs !== undefined) {
-                story_tabs.options.onShow = function (e) {
-                    window.location.hash = "live" + $(e).attr("id");
-                };
+            if (activetablink.attr("href").endsWith("story")) {
+                window.location.hash = "live" + $(".active", "#" + activetablink.attr("href").substring(1)).attr("href").substring(1);
+            } else {
+                window.location.hash = "live" + activetablink.attr("href").substring(1);
             }
-        } else {
-            collapsible.options.onOpenStart = function () {
-                if ($(this).data("floor") !== undefined) {
-                    window.location.hash = "floor" + $(this).data("floor");
-                } else {
-                    window.location.hash = "tower" + $(this).data("tower");
-                }
-            }.bind(this);
+
+            let activetab = $(activetablink.attr("href"), this.el);
+            if (activetab.hasClass("live-difficulty") && activetab.data("initialized") === undefined) {
+                activetab.data("initialized", 1);
+                initNoteMapInteractions(activetab);
+            }
+        };
+
+        tabs.options.onShow = function (e) {
+            if ($(e).hasClass("live-difficulty") && $(e).data("initialized") === undefined) {
+                $(e).data("initialized", 1);
+                initNoteMapInteractions($(e));
+            }
+
+            if ($(e).attr("id").endsWith("story")) {
+                M.Tabs.getInstance($(".tabs", e)[0]).updateTabIndicator();
+                window.location.hash = "live" + $(".active", e).attr("href").substring(1);
+            } else {
+                window.location.hash = "live" + $(e).attr("id");
+            }
+        };
+
+        if (story_tabs !== undefined) {
+            story_tabs.options.onShow = function (e) {
+                window.location.hash = "live" + $(e).attr("id");
+            };
         }
 
         collapsible.options.onCloseStart = function () {
             window.location.hash = currentPage;
         };
-
-        if ($(this).data("tower") === undefined) {
-            $(".live-difficulty", this).each(function () {
-            });
-        }
     });
 }
 
-function initNoteMapInteractions() {
+function initNoteMapInteractions(noteMap) {
     let selecting = false;
     // note measure
     // TODO: add support for touch events on mobile... how?
-    let notebar = $(".notebar", this);
+    let notebar = $(".notebar", noteMap);
     notebar.mousedown(function (e) {
         selecting = true;
         let selector = $("<div></div>").addClass("selection");
@@ -155,7 +151,7 @@ function initNoteMapInteractions() {
             selector.css({"left": startpos - notebarPos.left, "width": endpos - startpos});
         });
         body.mouseup(function () {
-            $(this).off("mousemove").off("mouseup");
+            body.off("mousemove").off("mouseup");
             selecting = false;
             selector.remove();
             tooltip.css({"left": 0, "top": 0});
@@ -163,8 +159,8 @@ function initNoteMapInteractions() {
     });
 
     // gimmick mouseover
-    let gimmickmarkers = $(".notebar .gimmick", this);
-    let gimmickinfos = $(".detailinfo .gimmick", this);
+    let gimmickmarkers = $(".notebar .gimmick", noteMap);
+    let gimmickinfos = $(".detailinfo .gimmick", noteMap);
     gimmickmarkers.on("mouseover touchstart", function () {
         if (selecting || $(this).hasClass("hidden")) return;
         let gi = $(this).data("gimmick");
@@ -185,8 +181,8 @@ function initNoteMapInteractions() {
     });
 
     // appeal chance mouseover
-    let acmarkers = $(".notebar .appealchance", this);
-    let acinfos = $(".detailinfo .appealchance", this);
+    let acmarkers = $(".notebar .appealchance", noteMap);
+    let acinfos = $(".detailinfo .appealchance", noteMap);
     acmarkers.on("mouseover touchstart", function () {
         if (selecting) return;
         let ai = $(this).data("ac");
