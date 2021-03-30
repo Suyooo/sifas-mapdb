5b6c4ffb0a67dd89fd9aee261a761dec5334b4ea
Suyooo <me@suyo.be>
2021-03-30 14:40:11 +0200

    Show Adv+ in notes ranking now that it counts for profile

--- top.js
+++ top.js
@@ -38,74 +38,100 @@ fs.readdirSync("mapdb/.").forEach(function (f) {
         }
         let lid = (json.live_id % 10000 + "").padStart(4, "0");
 
-        if (!songs_dict.hasOwnProperty(lid)) {
+        if (json.song_difficulty === 30) {
             songs_dict[lid] = {
                 "name": '<span class="translatable" data-rom="' + notemap.song_name_romaji(json.live_id) + '">' + json.song_name + '</span>',
+                "is_advplus": false,
+                "live_id": lid,
                 "attribute": json.song_attribute,
                 "length": json.song_length,
-                "notes": json.song_difficulty !== 35 ? json.notes.length : 0, // don't count Adv+ note counts
+                "notes": json.notes.length,
                 "is_available": isEventLive ? true : json.extra_info.is_available,
                 "can_show_on_profile": isEventLive ? false : json.extra_info.can_show_on_profile,
                 "linked_live_id": ldid
             };
-        } else {
-            if (json.song_difficulty !== 35 && json.notes.length > songs_dict[lid].notes) {
-                songs_dict[lid].notes = json.notes.length;
-                songs_dict[lid].linked_live_id = ldid;
-            }
-            if (!isEventLive) {
-                songs_dict[lid].is_available = songs_dict[lid].is_available || json.extra_info.is_available;
-                songs_dict[lid].can_show_on_profile = songs_dict[lid].can_show_on_profile || json.extra_info.can_show_on_profile;
-            }
+        } else if (json.song_difficulty === 35) {
+            songs_dict[lid+"plus"] = {
+                "name": '<span class="translatable" data-rom="' + notemap.song_name_romaji(json.live_id) + '">' + json.song_name + '</span> (Adv+)',
+                "is_advplus": true,
+                "live_id": lid,
+                "attribute": json.song_attribute,
+                "length": json.song_length,
+                "notes": json.notes.length,
+                "is_available": isEventLive ? true : json.extra_info.is_available,
+                "can_show_on_profile": isEventLive ? false : json.extra_info.can_show_on_profile,
+                "linked_live_id": ldid
+            };
         }
     }
 });
 
-/* Manual additions: songs that I was unable to dump before they disappeared - but I still know stuff! */
+/*
+Manual additions: songs that I was unable to dump before they disappeared - but I still know stuff!
+Only Adv note counts though - back then, Adv+ map was the same as Adv, no point is showing them twice in Rankings
+*/
 songs_dict["1080"] = {
     "name": '<span class="translatable" data-rom="New Romantic Sailors">New Romantic Sailors</span>',
+    "is_advplus": false,
+    "live_id": "1080",
     "attribute": 1,
     "length": 120838,
     "notes": 179,
     "is_available": false,
-    "can_show_on_profile": false
+    "can_show_on_profile": false,
+    "linked_live_id": null
 };
 songs_dict["1081"] = {
     "name": '<span class="translatable" data-rom="Braveheart Coaster">Braveheart Coaster</span>',
+    "is_advplus": false,
+    "live_id": "1081",
     "attribute": 4,
     "length": 120215,
     "notes": 182,
     "is_available": false,
-    "can_show_on_profile": false
+    "can_show_on_profile": false,
+    "linked_live_id": null
 };
 songs_dict["1082"] = {
     "name": '<span class="translatable" data-rom="Amazing Travel DNA">Amazing Travel DNA</span>',
+    "is_advplus": false,
+    "live_id": "1082",
     "attribute": 6,
     "length": 109635,
     "notes": 163,
     "is_available": false,
-    "can_show_on_profile": false
+    "can_show_on_profile": false,
+    "linked_live_id": null
 };
 songs_dict["2020"] = {
     "name": '<span class="translatable" data-rom="Love U my friends(2D)">Love U my friends(2D)</span>',
+    "is_advplus": false,
+    "live_id": "2020",
     "attribute": 4,
     "length": 105592,
     "notes": 176,
     "is_available": false,
-    "can_show_on_profile": false
+    "can_show_on_profile": false,
+    "linked_live_id": null
 };
 songs_dict["2061"] = {
     "name": '<span class="translatable" data-rom="Mirai Harmony">未来ハーモニー</span>',
+    "is_advplus": false,
+    "live_id": "2061",
     "attribute": 3,
     "length": 110058,
     "notes": 0, // Adv note count unknown as of now
     "is_available": false,
-    "can_show_on_profile": false
+    "can_show_on_profile": false,
+    "linked_live_id": null
 };
 
 /* Ignore songs for length ranking: Remove old versions of songs that became permanent later */
 songs_dict["2020"].length = 0;      // Love U my friends(2D)
 songs_dict["2040"].length = 0;      // 虹色Passions！
+
+/* Avoid mixups */
+songs_dict["2040"].name += " (2D)"; // 虹色Passions！
 
 let songs = Object.keys(songs_dict).map(function (e) {
     return songs_dict[e];
@@ -120,7 +146,10 @@ let rank_display = 1;
 let fullrank_display = 1;
 let last = -1;
 songs.filter(function (e) {
-    return e.length > 0;
+    if (e.length === 0) return false;
+    if (!e.is_advplus) return true;
+    if (songs_dict.hasOwnProperty(e.live_id)) return false; // only show Adv+ in timing if there is no Adv version
+    return true;
 }).sort(function (a, b) {
     return a.length - b.length;
 }).forEach(function (e) {
@@ -136,9 +165,16 @@ songs.filter(function (e) {
     if (rank > 11) classStr += " hide-if-narrow";
     if (classStr !== "") classStr = " class='" + classStr.trim() + "'";
 
+    let la = "";
+    let lb = "";
+    if (e.linked_live_id !== null) {
+        la = "<a href='.#live" + e.linked_live_id + "'>";
+        lb = "</a>";
+    }
+
     short += "<tr" + classStr + "><td><span class='notopen'>" + rank_display + "</span><span class='open'>" + fullrank_display +
         "</span></td><td style=\"background-image: url('image/icon_" + notemap.attribute(e.attribute) +
-        ".png')\">&nbsp;</td><td><a href='.#live" + e.linked_live_id + "'>" + e.name + "</a></td><td>" + min + ":" + (sec.toFixed(3) + "").padStart(6, "0") + "</td></tr>";
+        ".png')\">&nbsp;</td><td>" + la + e.name + lb + "</a></td><td>" + min + ":" + (sec.toFixed(3) + "").padStart(6, "0") + "</td></tr>";
     last = e.length;
 });
 
@@ -161,9 +197,16 @@ songs.filter(function (e) {
     if (rank > 11) classStr += " hide-if-narrow";
     if (classStr !== "") classStr = " class='" + classStr.trim() + "'";
 
+    let la = "";
+    let lb = "";
+    if (e.linked_live_id !== null) {
+        la = "<a href='.#live" + e.linked_live_id + "'>";
+        lb = "</a>";
+    }
+
     most += "<tr" + classStr + "><td><span class='notopen'>" + rank_display + "</span><span class='open'>" + fullrank_display +
         "</span></td><td style=\"background-image: url('image/icon_" + notemap.attribute(e.attribute) +
-        ".png')\">&nbsp;</td><td><a href='.#live" + e.linked_live_id + "'>" + e.name + "</a></td><td>" + e.notes + "</td></tr>";
+        ".png')\">&nbsp;</td><td>" + la + e.name + lb + "</td><td>" + e.notes + "</td></tr>";
     last = e.notes;
 });
 
