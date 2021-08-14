090dcf41401d413852d624be4c6530c742b152c7
Suyooo <me@suyo.be>
2021-08-14 15:43:55 +0200

    Add limited song expiry dates

--- settings.js
+++ settings.js
@@ -1,3 +1,11 @@
 module.exports = {
-    "current_event_live_ids": [21025]
+    "current_event_live_ids": [21025],
+    "limited_song_deadlines": {
+        12072: 1630389540,
+        12088: 1632981540,
+        12090: 1632981540,
+        12092: 1632981540,
+        13002: 1635659940,
+        13003: 1635659940
+    }
 };
