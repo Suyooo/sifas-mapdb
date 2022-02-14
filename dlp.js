4fd6fc8eb5577ce2b9ae132df592884dee9842fb
Suyooo <me@suyo.be>
2022-02-14 11:44:01 +0100

    Added: DLP "Sk Types on Stage" Floor Info

--- dlp.js
+++ dlp.js
@@ -41,6 +41,7 @@ function tower_name_romaji(tower_id) {
     if (tower_id === 33017) return "Dream Live Parade ~Gd Types On Stage~";
     if (tower_id === 33018) return "Dream Live Parade ~Vo Types On Stage~";
     if (tower_id === 33019) return "All Stars Special Dream Live Parade 2021";
+    if (tower_id === 33020) return "Dream Live Parade ~Sk Types On Stage~";
 
     throw new Error('Unknown Romaji Tower Name for ' + tower_id);
 }
