c8ae5c3cf7a8473d1f78a12ee8e7074028a0cef1
Suyooo <me@suyo.be>
2022-09-25 13:34:44 +0200

    Added: DLP Sep 2022 Part 4 Floor Info

--- dlp.js
+++ dlp.js
@@ -56,6 +56,7 @@ function towerNameRomaji(towerId) {
     if (towerId === 33027) return "Dream Live Parade - 3rd Anniversary (Part 1) -";
     if (towerId === 33028) return "Dream Live Parade - 3rd Anniversary (Part 2) -";
     if (towerId === 33029) return "Dream Live Parade - 3rd Anniversary (Part 3) -";
+    if (towerId === 33030) return "Dream Live Parade ~ 3rd Anniversary Next TOKIMEKI! ~";
 
     throw new Error('Unknown Romaji Tower Name for ' + towerId);
 }
