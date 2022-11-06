af72fab54eb3e9b3db8df8d2a8fe9451e41f92ee
Suyooo <me@suyo.be>
2022-11-06 18:59:23 +0100

    Force unmount to avoid leaving tooltips in DOM when custom animation is being weird

--- src/lib/tooltip.ts
+++ src/lib/tooltip.ts
@@ -5,7 +5,7 @@ import "tippy.js/dist/svg-arrow.css";
 
 const tippy = createTippy({
     arrow: roundArrow + roundArrow,
-    duration: 200,
+    duration: 150,
     onTrigger(instance) {
         const box = instance.popper.firstElementChild;
         requestAnimationFrame(() => {
@@ -15,6 +15,7 @@ const tippy = createTippy({
     onHide(instance) {
         const box = instance.popper.firstElementChild;
         box!.classList.remove("shown");
+        setTimeout(() => instance.unmount(), 150);
     }
 });
 
