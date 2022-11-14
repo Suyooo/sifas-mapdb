bda85c8fc61c7b62244618817c6118610b07d69e
Suyooo <me@suyo.be>
2022-11-14 12:53:47 +0100

    404 on invalid live IDs

--- src/routes/[[lang=lang]]/(map)/+layout.ts
+++ src/routes/[[lang=lang]]/(map)/+layout.ts
@@ -1,6 +1,8 @@
+import {error} from "@sveltejs/kit";
 import type {LayoutLoad} from "./$types";
 
 export const load: LayoutLoad = async ({ params, fetch }) => {
     const res = await fetch(`/json/live/${params.id}.json`);
+    if (!res.ok) throw error(res.status, res.statusText);
     return res.json();
 }
