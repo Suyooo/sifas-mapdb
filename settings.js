584dfc8ddfd449cca81fca3fbc026f2de10a7cac
Suyooo <me@suyo.be>
2023-04-30 13:54:39 +0200

    Added: Yuuki wa Doko ni? Kimi no Mune ni! (Permanent ver.)

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "active_event_live_ids": [21055], // first five digits as integer
+    "active_event_live_ids": [], // first five digits as integer
     "limited_song_deadlines": {}, // (mapping: live ID => unix timestamp in ms)
     "extra_live_difficulty_ids": [40002203, 40002301] // full live difficulty ids
 };
